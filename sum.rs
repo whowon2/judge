5305011914074739444af4f15eb1d83512209965
whowon2 <86119976+whowon2@users.noreply.github.com>
2026-03-19 21:29:36 -0300

    chore(deps): update Cargo.lock and add sum.rs playground file

--- sum.rs
+++ sum.rs
@@ -1,15 +1,17 @@
+use std::io;
 fn main() {
-    let mut buffer = String::new();
-    std::io::stdin()
-        .read_line(&mut buffer)
-        .expect("Failed to read line");
-
-    let nums: Vec<u32> = buffer
-        .split_whitespace()
-        .map(|s| s.parse::<u32>().expect("Expected a number"))
-        .collect();
-
-    let sum = nums[0] + nums[1];
-
-    println!("{}", sum);
+    let mut input = String::new();
+    
+    // Read the single line from standard input
+    if let Ok(_) = io::stdin().read_line(&mut input) {
+        // Split the string by whitespace and parse into integers
+        let sum: i64 = input
+            .trim()
+            .split_whitespace()
+            .filter_map(|s| s.parse::<i64>().ok())
+            .sum();
+            
+        // Print the result to standard out
+        println!("{}", sum);
+    }
 }
